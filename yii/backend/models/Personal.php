f882302ec968b58048f558654dafec91a4e9f2dc
qibite <qibite@mail.ru>
2018-01-15 17:07:16 +0900

    Добавлена табличка с обслуживающим персоналом, нужно устранить косяк с навешиванием нескольких событий на кнопку СОХРАНИТЬ при нескольких наведениях

---
+++ yii/backend/models/Personal.php
@@ -0,0 +1,14 @@
+<?php
+namespace backend\models;
+
+use yii\db\ActiveRecord;
+/**
+*  AjaxController
+*/
+class Personal extends ActiveRecord
+{
+	/*public static function tableName()
+	{
+		return 'services';
+	}*/
+}
