9e35cc16ecf32e25f4df79381f7fb97fafedd5ea
qibite <qibite@mail.ru>
2018-02-02 10:15:51 +0900

    Add new database structure with new table incident

---
+++ 02.02.2018.sql
@@ -0,0 +1,349 @@
+-- MySQL dump 10.14  Distrib 5.5.50-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: sauto
+-- ------------------------------------------------------
+-- Server version	5.5.50-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firm` char(50) NOT NULL,
+  `model` char(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `model` (`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES (1,'Acura','CL'),(2,'Acura','CSX'),(3,'Acura','EL'),(4,'Acura','ILX'),(5,'Acura','Integra'),(6,'Toyota','MR2'),(7,'Toyota','Corolla'),(8,'Honda','Partner'),(9,'Honda','Integra'),(10,'Toyota','Airwave'),(11,'Nissan','Cedric'),(12,'Honda','Accord'),(13,'Lamborgini','Diablo'),(14,'ГАЗ','3110'),(15,'ГАЗ','24'),(16,'Nissan','March'),(17,'Nissan','GTR'),(18,'Nissan','Avenir'),(19,'Ford','Focus'),(20,'Toyota','Corona'),(21,'ГАЗ','21'),(22,'Toyota','Cresta'),(23,'Toyota','Allion'),(24,'Toyota','Land Cruiser');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `family` char(50) NOT NULL,
+  `name` char(50) NOT NULL,
+  `surname` char(50) DEFAULT NULL,
+  `phone` char(50) NOT NULL,
+  `avto_id` smallint(6) NOT NULL,
+  `mail` char(50) DEFAULT NULL,
+  `gosno` char(50) NOT NULL,
+  `sor` char(50) NOT NULL,
+  `year` smallint(6) NOT NULL,
+  `summ` int(11) DEFAULT NULL,
+  `descont` int(11) DEFAULT NULL,
+  `history` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Сизиков','Александр','','+7 999 999-99-99',19,'684','9547','4684',2012,NULL,NULL,NULL),(2,'Иванов','Иван','Иванович','+7 999 999-99-99',24,'1@1.com','с845уа75','774455845',2012,NULL,NULL,NULL),(3,'Долженко','Алексей','Сергеевич','+7 999 999-99-99',23,'test@test.du','x777xx75rus','75AB462154',2009,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cwpt`
+--
+
+DROP TABLE IF EXISTS `cwpt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cwpt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `car` smallint(6) NOT NULL,
+  `work` smallint(6) NOT NULL,
+  `price` mediumint(9) NOT NULL,
+  `time` smallint(6) NOT NULL,
+  UNIQUE KEY `id` (`id`),
+  KEY `FK_car_work_price_cars` (`car`),
+  KEY `FK_car_work_price_services` (`work`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cwpt`
+--
+
+LOCK TABLES `cwpt` WRITE;
+/*!40000 ALTER TABLE `cwpt` DISABLE KEYS */;
+INSERT INTO `cwpt` VALUES (1,14,1,100,15);
+/*!40000 ALTER TABLE `cwpt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `incident`
+--
+
+DROP TABLE IF EXISTS `incident`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `incident` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_client` smallint(6) NOT NULL,
+  `id_car` smallint(6) NOT NULL,
+  `works` text NOT NULL,
+  `parts` text,
+  `master` varchar(50) NOT NULL,
+  `time_to_start` datetime NOT NULL,
+  `time_to_end_plane` datetime NOT NULL,
+  `time_t_end_real` datetime DEFAULT NULL,
+  `work_time` int(11) NOT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `price` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `incident`
+--
+
+LOCK TABLES `incident` WRITE;
+/*!40000 ALTER TABLE `incident` DISABLE KEYS */;
+INSERT INTO `incident` VALUES (1,1,19,'[\"19\",\"16\",\"17\",\"11\",\"10\",\"14\",\"1\",\"21\",\"6\",\"8\"]',NULL,'1','2018-02-02 09:30:00','2018-03-03 14:10:00',NULL,700,1,8100),(2,1,19,'[\"16\",\"15\",\"18\",\"12\",\"14\",\"4\",\"10\",\"17\",\"3\",\"19\",\"9\"]',NULL,'1','2018-01-01 09:45:00','2018-01-05 10:35:00',NULL,1010,1,13050),(3,3,23,'[\"22\",\"15\",\"22\",\"16\",\"12\",\"18\"]',NULL,'2','2018-01-02 11:00:00','2018-01-05 10:40:00',NULL,460,1,7650);
+/*!40000 ALTER TABLE `incident` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migration`
+--
+
+DROP TABLE IF EXISTS `migration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migration`
+--
+
+LOCK TABLES `migration` WRITE;
+/*!40000 ALTER TABLE `migration` DISABLE KEYS */;
+INSERT INTO `migration` VALUES ('m000000_000000_base',1507783346),('m130524_201442_init',1507783355);
+/*!40000 ALTER TABLE `migration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `optional_status`
+--
+
+DROP TABLE IF EXISTS `optional_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `optional_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` smallint(1) NOT NULL,
+  `descript_stat` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `optional_status`
+--
+
+LOCK TABLES `optional_status` WRITE;
+/*!40000 ALTER TABLE `optional_status` DISABLE KEYS */;
+INSERT INTO `optional_status` VALUES (1,1,'Создан'),(2,2,'Успешно завершен'),(3,0,'Отказано');
+/*!40000 ALTER TABLE `optional_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parts`
+--
+
+DROP TABLE IF EXISTS `parts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `car` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parts`
+--
+
+LOCK TABLES `parts` WRITE;
+/*!40000 ALTER TABLE `parts` DISABLE KEYS */;
+INSERT INTO `parts` VALUES (1,'Стойка FR','sh284hf',7,4370),(2,'Шаровая нижняя','21',23,850);
+/*!40000 ALTER TABLE `parts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fio` varchar(50) NOT NULL,
+  `busy` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (1,'Попов Василий Михайлович',0),(2,'Петухов Владимир Владимирович',0),(3,'Чистохин Евгений Петрович',1),(4,'Мальцев Михаил Сергеевич',0);
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `services` (
+  `id` int(99) NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  `code` tinyint(4) NOT NULL,
+  `raiting` int(11) NOT NULL DEFAULT '0',
+  `price` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `services`
+--
+
+LOCK TABLES `services` WRITE;
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+INSERT INTO `services` VALUES (1,'Снятие/установка колеса',3,100,100,10),(2,'Замена ступичного подшипника',7,100,1200,90),(3,'Замена масла',15,100,300,40),(4,'Заправка кондиционера',26,100,1200,15),(5,'Замена лобового стекла',28,100,1500,480),(6,'Компьютерная диагностика',8,100,800,20),(7,'Покраска двери FL',36,100,5500,4320),(8,'Замена лампочки',12,100,300,15),(9,'Замена масла в МКПП',6,100,500,180),(10,'Диагностика подвески',8,100,300,15),(11,'Замена АКБ',6,100,250,10),(12,'Чистка дроссельной заслонки',12,100,1000,30),(13,'Шумоизоляция моторного отсека',14,100,8000,2880),(14,'Замена ступицы в сборе',12,100,1000,120),(15,'Замена стойки',12,100,800,60),(16,'Перепрессовка сайлентблока',12,100,350,20),(17,'Замена радиатора',15,100,1500,100),(18,'Полировка кузова',0,100,2000,90),(19,'Замена АКПП',0,100,3000,360),(20,'Полировка фар',0,100,1500,60),(21,'Замена термостата',0,100,500,30),(22,'Замена свечи зажигания',0,100,100,10),(23,'Замена кислородного датчика',0,100,800,45),(24,'Замена ГРМ',0,100,3500,270);
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings` (
+  `start_day_h` char(10) DEFAULT '9',
+  `start_day_m` char(10) DEFAULT '00',
+  `start_obed_h` char(10) DEFAULT '13',
+  `start_obed_m` char(10) DEFAULT '00',
+  `end_obed_h` char(10) DEFAULT '14',
+  `end_obed_m` char(10) DEFAULT '00',
+  `end_day_h` char(10) DEFAULT '18',
+  `end_day_m` char(10) DEFAULT '00',
+  `weekend` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `settings`
+--
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+INSERT INTO `settings` VALUES ('9','00','13','00','14','00','18','00','03.02.2018,04.02.2018,10.02.2018,11.02.2018,17.02.2018,18.02.2018,24.02.2018,25.02.2018');
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','Uegz6okjEWQEY2U6Alxi1QRxq31aQAWN','$2y$13$GOE..kj1iL.Tfp/V85PEweoLN5xyRUFPMHYMk/Fds.bbnLAZorLAS',NULL,'qibite@mail.ru',10,1507785585,1507785585);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-02 10:15:05
